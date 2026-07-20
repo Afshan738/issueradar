75f1db062eb09ad752c00fc9db207d0e135a792e
Afshan738 <afshanqasim349@gmail.com>
2026-07-20 10:12:21 +0500

    Added the logging in the notifier

--- src/notifier.js
+++ src/notifier.js
@@ -1,11 +1,11 @@
-// const fetch = require("node-fetch");
-
 const NTFY_TOPIC = process.env.NTFY_TOPIC;
 
 async function sendNotification(issue) {
   const message = `${issue.title}\n${issue.html_url}`;
 
-  await fetch(`https://ntfy.sh/${NTFY_TOPIC}`, {
+  console.log(`Sending notification to ntfy topic: ${process.env.NTFY_TOPIC}`);
+
+  const response = await fetch(`https://ntfy.sh/${process.env.NTFY_TOPIC}`, {
     method: "POST",
     headers: {
       Title: `New Issue: ${issue.repository_url.split("/").slice(-1)[0]}`,
@@ -14,6 +14,7 @@ async function sendNotification(issue) {
     },
     body: message,
   });
-}
 
+  console.log(`ntfy response status: ${response.status}`);
+}
 module.exports = { sendNotification };
